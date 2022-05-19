10720b3563b8839bb43bcba8d45365818968429d
Diana Chiu <diana.chiu@sap.com>
2022-05-19 16:08:27 -0400

    initial code setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to Upgrade challenge</h1>
+      </header>
+      <p>
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+}
+
+export default App;
